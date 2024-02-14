a7ad42144f49f9bb660dcc240ba1a09ec5418bf1
shishirshettyyy <159223975+shishirshettyyy@users.noreply.github.com>
2024-02-13 20:26:02 -0800

    Update hello2.java

--- hello2.java
+++ hello2.java
@@ -1,11 +1,11 @@
 
-import java.util.Scanner; // import the Scanner class 
+import java.util.Scanner; 
 
 class hello2 {
   public static void main(String[] args) {
     
     
-    // Enter username and press Enter
+  
     System.out.println("Enter username"); 
 	Scanner myObj = new Scanner(System.in);
     String userName;
